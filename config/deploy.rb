9a70e316bd27fcdf97df2c3b8741117ef71a92b6
Christopher Pumihic <tophie.pumihic@gmail.com>
2017-07-14 16:01:56 +0800

    Upgrade ruby version and update commands for mina.

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,16 +3,17 @@ require 'mina/rails'
 require 'mina/git'
 require 'mina/rbenv'
 
-set :domain, '192.168.254.104'
+set :domain, '192.168.254.105'
 set :deploy_to, '/var/www/itworx_pos_final'
 set :repository, 'https://github.com/nnittop909/itworx_pos_final.git'
 set :branch, 'master'
 set :user, 'deploy'
 set :term_mode, nil
 set :forward_agent, true
-set :app_path, lambda { "#{deploy_to}/#{current_path}" }
+set :app_path, lambda { "#{fetch(:deploy_to)}/#{fetch(:current_path)}" }
 set :stage, 'production'
-set :shared_paths, ['config/database.yml', 'config/secrets.yml', 'log', 'tmp/log', 'public/system']
+set :shared_dirs, fetch(:shared_dirs, []).push('log', 'tmp/log', 'tmp/pids', 'tmp/sockets', 'public/system')
+set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml')
 # Optional settings:
 #   set :user, 'foobar'    # Username in the server to SSH to.
 #   set :port, '30000'     # SSH port number.
@@ -33,44 +34,43 @@ end
 # For Rails apps, we'll make some of the shared paths that are shared between
 # all releases.
 task :setup => :environment do
-  queue! %[mkdir -p "#{deploy_to}/#{shared_path}/log"]
-  queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/log"]
+  command %[mkdir -p "#{fetch(:deploy_to)}/#{fetch(:shared_path)}/log"]
+  command %[chmod g+rx,u+rwx "#{fetch(:deploy_to)}/#{fetch(:shared_path)}/log"]
 
-  queue! %[mkdir -p "#{deploy_to}/#{shared_path}/tmp/log"]
-  queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/tmp/log"]
+  command %[mkdir -p "#{fetch(:deploy_to)}/#{fetch(:shared_path)}/tmp/log"]
+  command %[chmod g+rx,u+rwx "#{fetch(:deploy_to)}/#{fetch(:shared_path)}/tmp/log"]
 
-  queue! %[mkdir -p "#{deploy_to}/#{shared_path}/config"]
-  queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/config"]
+  command %[mkdir -p "#{fetch(:deploy_to)}/#{fetch(:shared_path)}/config"]
+  command %[chmod g+rx,u+rwx "#{fetch(:deploy_to)}/#{fetch(:shared_path)}/config"]
 
-  queue! %[mkdir -p "#{deploy_to}/#{shared_path}/public/system"]
-  queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/public/system"]
+  command %[mkdir -p "#{fetch(:deploy_to)}/#{fetch(:shared_path)}/public/system"]
+  command %[chmod g+rx,u+rwx "#{fetch(:deploy_to)}/#{fetch(:shared_path)}/public/system"]
 
-  queue! %[mkdir -p "#{deploy_to}/shared/tmp/pids"]
-  queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/tmp"]
-  queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/tmp/pids"]
+  command %[mkdir -p "#{fetch(:deploy_to)}/shared/tmp/pids"]
+  command %[chmod g+rx,u+rwx "#{fetch(:deploy_to)}/shared/tmp"]
+  command %[chmod g+rx,u+rwx "#{fetch(:deploy_to)}/shared/tmp/pids"]
 
-  queue! %[mkdir -p "#{deploy_to}/shared/tmp/sockets"]
-  queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/tmp/sockets"]
+  command %[mkdir -p "#{fetch(:deploy_to)}/shared/tmp/sockets"]
+  command %[chmod g+rx,u+rwx "#{fetch(:deploy_to)}/shared/tmp/sockets"]
 
-  queue! %[touch "#{deploy_to}/#{shared_path}/tmp/log/stdout"]
-  queue! %[touch "#{deploy_to}/#{shared_path}/tmp/log/stderr"]
-  queue! %[touch "#{deploy_to}/#{shared_path}/tmp/pids/puma.pid"]
-  queue! %[touch "#{deploy_to}/#{shared_path}/tmp/sockets/puma.sock"]
-  queue! %[touch "#{deploy_to}/#{shared_path}/config/database.yml"]
-  queue! %[touch "#{deploy_to}/#{shared_path}/config/secrets.yml"]
-  queue  %[echo "-----> Be sure to edit '#{deploy_to}/#{shared_path}/config/database.yml' and 'secrets.yml'."]
+  command %[touch "#{fetch(:deploy_to)}/#{fetch(:shared_path)}/tmp/log/stdout"]
+  command %[touch "#{fetch(:deploy_to)}/#{fetch(:shared_path)}/tmp/log/stderr"]
+  command %[touch "#{fetch(:deploy_to)}/#{fetch(:shared_path)}/tmp/pids/puma.pid"]
+  command %[touch "#{fetch(:deploy_to)}/#{fetch(:shared_path)}/tmp/sockets/puma.sock"]
+  command %[touch "#{fetch(:deploy_to)}/#{fetch(:shared_path)}/config/database.yml"]
+  command %[touch "#{fetch(:deploy_to)}/#{fetch(:shared_path)}/config/secrets.yml"]
+  comment  %[echo "-----> Be sure to edit '#{fetch(:deploy_to)}/#{fetch(:shared_path)}/config/database.yml' and 'secrets.yml'."]
 
-  if repository
-    repo_host = repository.split(%r{@|://}).last.split(%r{:|\/}).first
-    repo_port = /:([0-9]+)/.match(repository) && /:([0-9]+)/.match(repository)[1] || '22'
+  # if repository
+  #   repo_host = repository.split(%r{@|://}).last.split(%r{:|\/}).first
+  #   repo_port = /:([0-9]+)/.match(repository) && /:([0-9]+)/.match(repository)[1] || '22'
 
-    queue %[
-      if ! ssh-keygen -H  -F #{repo_host} &>/dev/null; then
-        ssh-keyscan -t rsa -p #{repo_port} -H #{repo_host} >> ~/.ssh/known_hosts
-      fi
-    ]
-  end
-end
+  #   command %[
+  #     if ! ssh-keygen -H  -F #{repo_host} &>/dev/null; then
+  #       ssh-keyscan -t rsa -p #{repo_port} -H #{repo_host} >> ~/.ssh/known_hosts
+  #     fi
+  #   ]
+  # end
 
 #########################################
 desc "Deploys the current version to the server."
@@ -85,7 +85,7 @@ task :deploy => :environment do
     invoke :'rails:assets_precompile'
     invoke :'deploy:cleanup'
 
-    to :launch do
+    on :launch do
       invoke :'puma:restart'
     end
   end
@@ -95,19 +95,19 @@ namespace :puma do
   desc "Start the application"
   task :start do
     queue 'echo "-----> Start Puma"'
-    queue "cd #{app_path} && RAILS_ENV=#{stage} && bin/puma.sh start"
+    queue "cd #{fetch(:app_path)} && RAILS_ENV=#{fetch(:stage)} && bin/puma.sh start"
   end
 
   desc "Stop the application"
   task :stop do
     queue 'echo "-----> Stop Puma"'
-    queue "cd #{app_path} && RAILS_ENV=#{stage} && bin/puma.sh stop"
+    queue "cd #{fetch(:app_path)} && RAILS_ENV=#{fetch(:stage)} && bin/puma.sh stop"
   end
 
   desc "Restart the application"
   task :restart do
     queue 'echo "-----> Restart Puma"'
-    queue "cd #{app_path} && RAILS_ENV=#{stage} && bin/puma.sh restart"
+    queue "cd #{fetch(:app_path)} && RAILS_ENV=#{fetch(:stage)} && bin/puma.sh restart"
   end
 end
 
@@ -115,6 +115,9 @@ namespace :deploy do
   desc "reload the database with seed data"
   task :seed do
     invoke :'rbenv:load'
-    queue "cd #{app_path}; bundle exec rails db:seed RAILS_ENV=#{stage}"
+    queue "cd #{fetch(:app_path)}; bundle exec rails db:seed RAILS_ENV=#{fetch(:stage)}"
   end
 end
+end
+
+
